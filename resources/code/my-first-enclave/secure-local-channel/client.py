3148820996737304b13be65a3b7f246a76d1b9a1
Anton Gridin <hryanton@amazon.com>
2021-07-06 18:54:30 -0400

    Adding liciencing headers.

--- resources/code/my-first-enclave/secure-local-channel/client.py
+++ resources/code/my-first-enclave/secure-local-channel/client.py
@@ -1,3 +1,6 @@
+# // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# // SPDX-License-Identifier: MIT-0
+
 #!/usr/local/bin/env python3
 import argparse
 import socket
--- resources/code/my-first-enclave/secure-local-channel/server.py
+++ resources/code/my-first-enclave/secure-local-channel/server.py
@@ -1,3 +1,5 @@
+# // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# // SPDX-License-Identifier: MIT-0
 import argparse
 import socket
 import sys
